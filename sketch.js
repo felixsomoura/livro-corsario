ff05be47696d29c2ffb99cab5ad1836791d432ff
felixsomoura <168107996+felixsomoura@users.noreply.github.com>
2024-04-25 11:49:37 -0300

    Upload de Files com Primeiro Teste de Params

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var tabela;
+//var url = document.referrer;
+
+function preload() {
+  tabela =
+    loadTable ("SRD_Spells_TCC.csv","csv","header");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  
+  //console.log(url);
+  //getURL();
+  //getURLParams();
+  //var params = getURLParams();
+  
+  background(29, 29, 27);
+  
+  var divCaixa = createDiv('');
+  divCaixa.position(0, 0);
+  
+  if (windowWidth >= 500) {
+       divCaixa.addClass('caixaBig');
+      }
+  
+  if (windowWidth <= 500) {
+       divCaixa.addClass('caixaSmall');
+      }
+  
+  else {}
+  
+  divCaixa.addClass('caixa');
+  divCaixa.center('horizontal'); 
+  
+  pTitulo = createP(tabela.get(81,0));
+  pTitulo.addClass('titulo');   
+  pTitulo.parent(divCaixa);
+  
+  pCategoria = createP('Magia de' + " " + tabela.get(81,3));
+  pCategoria.addClass('categoria');   
+  pCategoria.parent(divCaixa);
+  
+  hrLinha = createP('');
+  hrLinha.addClass('hr');   
+  hrLinha.parent(divCaixa);
+  
+  pCorpo = createP(tabela.get(81,14));
+  pCorpo.addClass('corpo');   
+  pCorpo.parent(divCaixa);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
